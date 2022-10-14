4d230cd06a548cb2230eb51da97e396e2b6a5248
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2022-10-14 19:35:26 +0300

    Add files via upload

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+function pressButtonNext() {
+    let elem = document.getElementById('pic'); 
+    elem.style.backgroundImage = "url('./assets/images/2.jpg')";
+}
+
+function pressButtonPrev() {
+    elem = document.getElementById('pic'); 
+    elem.style.backgroundImage = "url('./assets/images/1.jpg')";
+}
